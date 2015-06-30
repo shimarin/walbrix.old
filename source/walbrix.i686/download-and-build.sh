412a7b76be085e22b37f28dc6e89a7ebf9c2d21b
Tomoatsu Shimada <shimada@walbrix.com>
2015-06-30 16:06:39 +0900

    stop assuming . as intended root dir

--- source/walbrix.i686/download-and-build.sh
+++ source/walbrix.i686/download-and-build.sh
@@ -7,6 +7,6 @@ fi
 ROOTDIR=`dirname $0`
 CHROOT="../../chroot ${ROOTDIR}"
 wget -O - `../../find_latest_stage3 i686` | tar jxvpf - -C "${ROOTDIR}" -k -P
-sed -i 's/^\(CFLAGS=.\+\)"$/\1 -mno-tls-direct-seg-refs"/' ./etc/portage/make.conf
+sed -i 's/^\(CFLAGS=.\+\)"$/\1 -mno-tls-direct-seg-refs"/' ${ROOTDIR}/etc/portage/make.conf
 $CHROOT emerge -uDN world || exit 1
 #$CHROOT "eselect python set python2.7"
