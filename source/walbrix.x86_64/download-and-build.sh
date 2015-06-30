eb040e858a1358faea1a0b23b968dea6a3de6b58
Tomoatsu Shimada <shimada@walbrix.com>
2015-06-30 16:38:06 +0900

    unnecessary emerging kernel prior to world

--- source/walbrix.x86_64/download-and-build.sh
+++ source/walbrix.x86_64/download-and-build.sh
@@ -7,6 +7,5 @@ fi
 ROOTDIR=`dirname $0`
 CHROOT="../../chroot ${ROOTDIR}"
 wget -O - `../../find_latest_stage3` | tar jxvpf - -C "${ROOTDIR}" -k -P
-$CHROOT emerge gentoo-sources || exit 1
 $CHROOT emerge -uDN world || exit 1
 #$CHROOT "eselect python set python2.7"
