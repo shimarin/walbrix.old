d927b74c015e83cfce36e70c0afb71fad2470522
Tomoatsu Shimada <shimada@walbrix.com>
2015-07-03 22:10:05 +0900

    owncloud added (some issues still remains at memcache/imagemagick, libreoffice still missing)

---
+++ files/owncloud/config.php
@@ -0,0 +1,20 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ocqi8s5fkpvb',
+  'passwordsalt' => 'Kp63L8WjA8R9OXdmKFJBMlIIDmiu4G',
+  'secret' => 'o5imdLguJpWGV/3PaV7hbAtNlbs2KmF1BlG6h5k+G0gktQHI',
+  'trusted_domains' => 
+  array (
+  ),
+  'datadirectory' => '/var/www/localhost/htdocs/data',
+  'overwrite.cli.url' => 'http://localhost',
+  'dbtype' => 'mysql',
+  'version' => '8.1.0.8',
+  'dbname' => 'owncloud',
+  'dbhost' => 'localhost',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'owncloud',
+  'dbpassword' => '',
+  'logtimezone' => 'UTC',
+  'installed' => true,
+);
---
+++ files/owncloud/initial-database-custom.sql
@@ -0,0 +1,1 @@
+replace into oc_appconfig(appid,configkey,configvalue) values('documents','converter','local')
