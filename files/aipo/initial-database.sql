6651318e39698c52b86dad3d0c7402f9c33604a5
Tomoatsu Shimada <shimada@walbrix.com>
2015-07-04 13:23:29 +0900

    aipo WIP

---
+++ files/aipo/initial-database.sql
@@ -0,0 +1,5 @@
+UPDATE EIP_M_COMPANY SET PORT_INTERNAL=80;
+UPDATE AIPO_LICENSE SET LIMIT_USERS=0;
+INSERT INTO eip_m_mail_account VALUES (1, 1, 'aipo', '1', 'localhost', '', '', '\x7a540f5868b3e117', 'aipo', 'aipo@example.com', '25', 0, '110', 0, 0, '', '\x7a540f5868b3e117', 0, '0', '0', 0, '1', 1, NULL, NULL, now(), now());
+
+
