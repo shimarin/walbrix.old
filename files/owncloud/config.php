27e8160dd879897ad68f5d7b3e083394141828da
Tomoatsu Shimada <shimada@walbrix.com>
2015-07-07 20:29:19 +0900

    closes #4

--- files/owncloud/config.php
+++ files/owncloud/config.php
@@ -3,9 +3,7 @@ $CONFIG = array (
   'instanceid' => 'ocqi8s5fkpvb',
   'passwordsalt' => 'Kp63L8WjA8R9OXdmKFJBMlIIDmiu4G',
   'secret' => 'o5imdLguJpWGV/3PaV7hbAtNlbs2KmF1BlG6h5k+G0gktQHI',
-  'trusted_domains' => 
-  array (
-  ),
+  'trusted_domains' => array ($_SERVER['SERVER_NAME']),
   'datadirectory' => '/var/www/localhost/htdocs/data',
   'overwrite.cli.url' => 'http://localhost',
   'memcache.local' => '\OC\Memcache\APCu',
