8bb4f48a7190b28d23ecc6b55a92437d00370cc9
Tomoatsu Shimada <shimada@walbrix.com>
2015-07-08 13:05:29 +0900

    closes #5

--- files/owncloud/config.php
+++ files/owncloud/config.php
@@ -1,19 +1,5 @@
 <?php
 $CONFIG = array (
-  'instanceid' => 'ocqi8s5fkpvb',
-  'passwordsalt' => 'Kp63L8WjA8R9OXdmKFJBMlIIDmiu4G',
-  'secret' => 'o5imdLguJpWGV/3PaV7hbAtNlbs2KmF1BlG6h5k+G0gktQHI',
-  'trusted_domains' => array ($_SERVER['SERVER_NAME']),
-  'datadirectory' => '/var/www/localhost/htdocs/data',
-  'overwrite.cli.url' => 'http://localhost',
   'memcache.local' => '\OC\Memcache\APCu',
-  'dbtype' => 'mysql',
-  'version' => '8.1.0.8',
-  'dbname' => 'owncloud',
-  'dbhost' => 'localhost',
-  'dbtableprefix' => 'oc_',
-  'dbuser' => 'owncloud',
-  'dbpassword' => '',
-  'logtimezone' => 'UTC',
-  'installed' => true,
+  'trusted_domains' => array ('SERVER_NAME_HERE'),
 );
