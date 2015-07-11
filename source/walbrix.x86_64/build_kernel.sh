38b3644e505a11eab64269fd41019b1d67f08cca
Tomoatsu Shimada <shimada@walbrix.com>
2015-07-11 16:40:36 +0900

    make sure appropriate kernel config exists when building kernel

--- source/walbrix.x86_64/build_kernel.sh
+++ source/walbrix.x86_64/build_kernel.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+KERNEL_NAME=`readlink /usr/src/linux | sed 's/linux-//'`
+KERNEL_CONFIG=/etc/kernels/kernel-config-x86_64-$KERNEL_NAME
+if [ ! -f "$KERNEL_CONFIG" ];then
+	echo "Kernel config file '$KERNEL_CONFIG' doesn't exist."
+	exit 1
+fi
 genkernel --no-mountboot --lvm --mdadm --symlink --splash=natural_gentoo all
 
