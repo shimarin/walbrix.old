02835bb091e82f4ce1cade639d9481119686b598
Tomoatsu Shimada <shimada@walbrix.com>
2015-07-16 09:35:07 +0900

    Issue #27 WIP

--- wbui/src/cli2/boot_vga_info.py
+++ wbui/src/cli2/boot_vga_info.py
@@ -1,4 +1,4 @@
-import glob,os,json
+import glob,os,json,subprocess,re
 
 def get_boot_vga():
     return (map(lambda x:os.path.dirname(x), filter(lambda x:open(x).read().strip() == "1", glob.glob("/sys/bus/pci*/devices/*/boot_vga"))) or [None])[0]
@@ -11,8 +11,23 @@ def get_boot_vga_info():
         filename = "%s/%s" % (vga_info, filename)
         if not os.path.isfile(filename): return None
         return open(filename).read().strip()
-    
-    return {"device_dir":vga_info, "vendor_id":read("vendor"), "device_id":read("device")}
+
+    vendor_id, device_id = (read("vendor"), read("device"))
+    info = {"device_dir":vga_info, "vendor_id":vendor_id, "device_id":device_id}
+
+    try:
+        lspci = subprocess.check_output(["lspci","-k","-d","%s:%s" % (vendor_id,device_id)])
+        modules_match = re.search(r'^\s*Kernel modules: ([a-z0-9_\-]+)', lspci, re.M)
+        module = modules_match and modules_match.groups()[0]
+        if module is not None:
+            info["module"] = module
+            modinfo = subprocess.check_output(["modinfo",module,"-p"])
+            info["modeset"] = re.search(r'^modeset:', modinfo, re.M) is not None
+    except OSError:
+        print "Failed to execute lspci or modinfo"
+    except subprocess.CalledProcessError:
+        print "lspci or modinfo failed(exit status != 0)"
+    return info
 
 if __name__ == '__main__':
     print json.dumps(get_boot_vga_info())
