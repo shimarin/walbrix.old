7acc4c26e8a2686769204ac155bd0ddf99d96569
Tomoatsu Shimada <shimada@walbrix.com>
2015-12-08 19:59:18 +0900

    PHP 5.5 / ownCloud 8.2.1

--- files/owncloud/config.php
+++ files/owncloud/config.php
@@ -1,5 +1,11 @@
 <?php
 $CONFIG = array (
-  'memcache.local' => '\OC\Memcache\APCu',
+  'memcache.local' => '\\OC\\Memcache\\Redis',
+  'filelocking.enabled' => 'true',
+  'memcache.locking' => '\\OC\\Memcache\\Redis',
+  'redis' => 
+  array (
+    'timeout' => 0.0,
+  ),
   'trusted_domains' => array ('SERVER_NAME_HERE'),
 );
