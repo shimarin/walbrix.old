7f03ec79bb94c5699d8c6f75a1861796fa2c596a
Tomoatsu Shimada <shimada@walbrix.com>
2017-02-06 12:47:22 +0900

    zabbix VA added

---
+++ files/zabbix/setup_initial_database.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+MYSQL="/usr/bin/mysql --socket=$MYSQL_UNIX_PORT"
+$MYSQL -u root -e "create database zabbix" || exit 1
+$MYSQL -u root -e 'grant all privileges on zabbix.* to zabbix@localhost' || exit 1
+$MYSQL -u zabbix zabbix < /usr/share/zabbix/database/mysql/schema.sql || exit 1
+$MYSQL -u zabbix zabbix < /usr/share/zabbix/database/mysql/images.sql || exit 1
+$MYSQL -u zabbix zabbix < /usr/share/zabbix/database/mysql/data.sql || exit 1
+$MYSQL -u zabbix zabbix -e "update users set lang='ja_JP'" || exit 1
+$MYSQL -u zabbix zabbix	-e "update media_type set smtp_server='localhost',smtp_helo='example.com',smtp_email='zabbix@example.com' where type=0" || exit 1
+$MYSQL -u zabbix zabbix -e "update hosts set status=0 where host='Zabbix server'" || exit 1
+
