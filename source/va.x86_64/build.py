7a86a87a3375d17353a173a54ca5b795246b9c93
Tomoatsu Shimada <shimada@walbrix.com>
2017-02-23 12:43:56 +0900

    Docker w/overlayfs root succeeded aufs kernel removed from va

--- source/va.x86_64/build.py
+++ source/va.x86_64/build.py
@@ -94,5 +94,5 @@ exec_cmd(["emerge","@preserved-rebuild"])
 ## build sub kernels
 
 build_kernel_if_needed("", ["bzImage"])
-build_kernel_if_needed("aufs", ["bzImage"])
+#build_kernel_if_needed("aufs", ["bzImage"])
 
