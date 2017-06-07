d48fa114a377495fa5a5ec417cea79181da16756
Tomoatsu Shimada <shimada@walbrix.com>
2017-06-07 18:56:43 +0900

    correct defining route though it should have been working before

--- files/ppp/ip-up.route.sh
+++ files/ppp/ip-up.route.sh
@@ -2,5 +2,5 @@
 PPP_IFACE="$1"
 PPP_LOCAL="$4"
 TABLE=$((250+`echo $PPP_IFACE|sed 's/^ppp//'`))
-ip route add dev $PPP_IFACE table $TABLE
+ip route add default dev $PPP_IFACE table $TABLE
 ip rule add from $PPP_LOCAL table $TABLE pref 100
