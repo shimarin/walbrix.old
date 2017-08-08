6a99992755c367143618b256e1ca61e70ca0160c
Tomoatsu Shimada <shimada@walbrix.com>
2017-08-08 20:13:32 +0900

    domU x86_64 overlayfs kernel 4.12.5 change how to handle amd and nvidia proprietary drivers

--- source/va.x86_64/build.py
+++ source/va.x86_64/build.py
@@ -100,5 +100,6 @@ exec_cmd(["emerge","@preserved-rebuild"])
 build_kernel_if_needed("", ["bzImage"])
 #build_kernel_if_needed("aufs", ["bzImage"])
 
-exec_cmd("tar zcvf /root/nvidia-cuda-toolkit.tar.gz -C / `equery files -f obj,sym,conf,cmd nvidia-cuda-toolkit | sed 's/ -> .*$//' | egrep -v '^\/usr\/share'`")
+exec_cmd("tar zcvf /root/nvidia-drivers.tar.gz -C / `equery files -f obj,sym,conf,cmd nvidia-drivers | sed 's/ -> .*$//' | egrep -v '^\/usr\/share' | egrep -v '^\/lib\/modules'`")
+exec_cmd("tar zcvf /root/amdgpu-pro-opencl.tar.gz -C / `equery files -f obj,sym,conf,cmd amdgpu-pro-opencl | sed 's/ -> .*$//'`")
 
