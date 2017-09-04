aeda006de5023f563c571dbfa3219076d5c64fb9
Tomoatsu Shimada <shimada@walbrix.com>
2017-09-04 18:32:48 +0900

    dom0 kernel 4.13.0

--- source/walbrix.x86_64/build.py
+++ source/walbrix.x86_64/build.py
@@ -84,7 +84,7 @@ def build_kernel_if_needed(source = "gentoo", genkernel_opts=[]):
 exec_cmd(["emerge","-uDN","gentoo-sources","genkernel","splash-themes-gentoo"])
 if build_kernel_if_needed("gentoo", ["--lvm","--mdadm","--symlink","--splash=natural_gentoo","all"]):
     try:
-        exec_cmd(["emerge","-1","spl","zfs-kmod","nvidia-drivers"])
+        exec_cmd(["emerge","-1","--keep-going","spl","zfs-kmod","nvidia-drivers"])
     except subprocess.CalledProcessError:
         print "Looks like ZFS or NVIDIA modules are not compatible with this kernel."
 
