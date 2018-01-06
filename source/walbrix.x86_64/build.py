c20ca449d2cf86445503544cb28aae867972da7f
Tomoatsu Shimada <shimada@walbrix.com>
2018-01-06 17:55:32 +0900

    revert xen-netback change which might have been cause of crash

--- source/walbrix.x86_64/build.py
+++ source/walbrix.x86_64/build.py
@@ -58,7 +58,6 @@ for user in users:
     useradd_if_not_exists(user[0], user[1], user[2], user[3], user[4],user[5])
 
 ## emerge/build main kernel
-
 def build_kernel_if_needed(source = "gentoo", genkernel_opts=[]):
     source = "-" + source if source not in ["", None] else ""
     for kernel_config in glob.glob("/etc/kernels/kernel-config-*"):
@@ -77,6 +76,11 @@ def build_kernel_if_needed(source = "gentoo", genkernel_opts=[]):
                     exec_cmd("wget -O - http://git.kernel.org/cgit/linux/kernel/git/tip/tip.git/patch/?id=8c058b0b9c34d8c8d7912880956543769323e2d8 | patch -d %s -p1 -R" % kerneldir)
                 else:
                     print "i8259 patch has already been reverted."
+                print "Reverting xen_netback patch cc8737a5"
+                if subprocess.call(["grep","-q","{ { .ctx = NULL,", "%s/drivers/net/xen-netback/interface.c" % kerneldir],shell=False) == 0:
+                    exec_cmd("wget -O - https://git.kernel.org/pub/scm/linux/kernel/git/tip/tip.git/patch/?id=cc8737a5fe9051b7fa052b08c57ddb9f539c389a | patch -d %s -p1 -R" % kerneldir)
+                else:
+                    print "xen_netback patch has already been reverted."
             exec_cmd(["genkernel","--no-mountboot","--kerneldir=%s" % kerneldir] + genkernel_opts)
             return True
     return False
