30363a5b8335c969ae76e4b4d2db80abe234bde6
Tomoatsu Shimada <shimada@walbrix.com>
2018-07-19 08:36:40 +0900

    cpu_mhz can be float

--- wbui/src/cli2/info.py
+++ wbui/src/cli2/info.py
@@ -1,6 +1,7 @@
 import subprocess,re
 
-INT_FIELDS = set(["nr_cpus", "max_cpu_id", "nr_nodes", "cores_per_socket", "threads_per_core", "cpu_mhz", "total_memory", "free_memory", "free_cpus", "xen_major", "xen_minor", "xen_pagesize", "xend_config_format"])
+INT_FIELDS = set(["nr_cpus", "max_cpu_id", "nr_nodes", "cores_per_socket", "threads_per_core", "total_memory", "free_memory", "free_cpus", "xen_major", "xen_minor", "xen_pagesize", "xend_config_format"])
+FLOAT_FIELDS = set(["cpu_mhz"])
 
 def run():
     pattern = re.compile(r'^(\S+)\s*:\s*(.*)$')
@@ -9,7 +10,9 @@ def run():
         match = pattern.search(line)
         if match is None: continue
         key, value = match.groups()
-        vals[key] = int(value) if key in INT_FIELDS else value
+        if key in FLOAT_FIELDS: value = float(value)
+        if key in INT_FIELDS: value = int(value)
+        vals[key] = value
 
     return vals
 
