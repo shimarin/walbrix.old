7236b67b073cdaaa41eb6e92b709a335209bfa91
Tomoatsu Shimada <shimada@walbrix.com>
2018-07-20 10:33:20 +0900

    path may not be file. ignore dirs

--- lib/cleanup_distfiles.py
+++ lib/cleanup_distfiles.py
@@ -12,9 +12,10 @@ try:
         if len(line) < 9: continue
         keep_files.add(line[8].strip())
     for present_file in os.listdir(distfiles):
-        if present_file not in keep_files:
+        full_path = os.path.join(distfiles, present_file)
+        if present_file not in keep_files and os.path.isfile(full_path):
             print "Removing %s..." % present_file
-            os.unlink(os.path.join(distfiles, present_file))
+            os.unlink(full_path)
 finally:
     os.unlink(".listing")
 
