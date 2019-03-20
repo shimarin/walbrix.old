51f501edc3c09fc587bfc1741de4e3af18aa44cc
Tomoatsu Shimada <shimada@walbrix.com>
2019-03-20 12:24:58 +0900

    dom0 kernel 5.0.3 add tools option to nvidia-drivers

--- lib/collect/execute.py
+++ lib/collect/execute.py
@@ -30,17 +30,19 @@ def do_chroot(target_dir, command, envvars={}):
     with bind_mount_if_both_exist("/proc", "%s/proc" % target_dir):
         with bind_mount_if_both_exist("/dev", "%s/dev" % target_dir):
             with bind_mount_if_both_exist("/usr/portage", "%s/usr/portage" % target_dir):
-                subprocess.check_call(["chroot", target_dir, "/bin/sh", "-c", command], env=env)
+                return subprocess.check_output(["chroot", target_dir, "/bin/sh", "-c", command], env=env)
 
 def apply(context, args):
     parser = argparse.ArgumentParser()
     parser.add_argument("--overlay", action="store_true", help="use overlay")
     parser.add_argument("--envvar", type=str, action="append", default=[], help="variable in NAME=VALUE format")
+    parser.add_argument("--store", type=str, default=None, help="variable name to store result")
     parser.add_argument("command", type=str, help="command to execute inside chroot")
     args = parser.parse_args(args)
     envvars = dict(map(lambda x:collect.parse_var(x), args.envvar))
     command = context.apply_variables(args.command)
     print "$exec '%s'" % command
+    output = ""
     if args.overlay:
         print "Using overlay"
         overlay_dir = os.path.normpath(context.destination) + ".overlay"
@@ -52,10 +54,13 @@ def apply(context, args):
             overlayfs_opts = "lowerdir=%s,upperdir=%s,workdir=%s" % (context.source,context.destination,overlay_work)
             subprocess.check_call(["mount","-t","overlay","overlay","-o",overlayfs_opts,overlay_root])
             try:
-                do_chroot(overlay_root, command, envvars)
+                output = do_chroot(overlay_root, command, envvars)
             finally:
                 subprocess.check_call(["umount", overlay_root])
         finally:
             shutil.rmtree(overlay_dir)
     else:
-        do_chroot(context.destination, command, envvars)
+        output = do_chroot(context.destination, command, envvars)
+
+    if args.store is not None:
+        context.set_variable(args.store, output.strip())
