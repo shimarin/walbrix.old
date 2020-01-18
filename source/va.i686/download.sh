0659fc2136d291836b4694ccd4540b61faebb433
Tomoatsu Shimada <shimada@walbrix.com>
2020-01-19 01:26:52 +0900

    remove obsoleted subtree

--- source/va.i686/download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
-   exit 1
-fi
-
-ROOTDIR=`dirname $0`
-wget -O - `${ROOTDIR}/../../find_latest_stage3 i686` | tar jxvpf - -C "${ROOTDIR}" -k -P
-
