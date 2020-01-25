ed16fa3d3d85dd3450f157aa8f4fcf3682a07874
Tomoatsu Shimada <shimada@walbrix.com>
2020-01-25 18:18:25 +0900

    script improved

--- resource/gpgpu/xmr-start.sh
+++ resource/gpgpu/xmr-start.sh
@@ -13,7 +13,9 @@ wrmsr -a 0xc0011022 0x510000
 wrmsr -a 0xc001102b 0x1808cc16
 wrmsr -a 0xc0011020 0
 
+echo 3 > /sys/kernel/mm/hugepages/hugepages-1048576kB/nr_hugepages
+
 while true; do
         ./xmr-stak-rx --currency monero -o $XMR_POOL -u $XMR_ADDR.$WORKER_NAME -p x --noNVIDIA --noAMD --noTest
-        [ $? -ne 139 -a $? -ne 132 ] && break
+        [ $? -lt 128 ] && break
 done
