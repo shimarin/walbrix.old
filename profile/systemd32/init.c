6dad45f37db534231c094d86c8aa9cdc16ad5ec0
Tomoatsu Shimada <shimada@walbrix.com>
2020-02-16 11:17:25 +0900

    router artifact added

--- profile/systemd32/init.c
+++ profile/systemd32/init.c
@@ -2,101 +2,15 @@
 #include "init.h"
 
 #define PROBE_BOOT_PARTITION_MAX_RETRY 8
-#define FIRMWARE_ARCHIVE "/run/initramfs/boot/efi/boot/firmware.tgz"
 
 void setup(inifile_t ini)
 {
   const char *hostname = ini_string(ini, ":hostname", NULL);
-  const char *password = ini_string(ini, ":password", NULL);
   const char *timezone = ini_string(ini, ":timezone", NULL);
   const char *keymap = ini_string(ini, ":keymap", NULL);
   const char *wifi_ssid = ini_string(ini, ":wifi_ssid", NULL);
   const char *wifi_key = ini_string(ini, ":wifi_key", "");
-  const int persistent = ini_bool(ini, ":persistent", 0);
-  const int compress = ini_bool(ini, ":compress", 1);
-  const int swap = ini_bool(ini, ":swap", 0);
   const int debug = ini_bool(ini, ":debug", 0);
-  const char *wg_endpoint = ini_string(ini, "wireguard:endpoint", NULL);
-  const char *wg_privkey = ini_string(ini, "wireguard:private_key", "");
-  const char *wg_pubkey = ini_string(ini, "wireguard:public_key", "");
-
-  if (debug) {
-    puts(">>>>setup");
-    sleep(3);
-  }
-
-  if (persistent) {
-    const char* datafile = "/mnt/boot/efi/boot/bootx86.dat";
-    if (!exists(datafile)) {
-      if (get_free_disk_space("/mnt/boot") >= (uint64_t)1024*1024*1024*2 ) {
-        printf("RW layer does not exist. Creating...");fflush(stdout);
-        if (create_btrfs_imagefile(datafile, 1024*1024*1024) == 0) {
-          printf("done.\n");
-        } else {
-          printf("failed.\n");
-          if (debug) sleep(3);
-        }
-      } else {
-        printf("No sufficient disk space to create RW layer.");
-        if (debug) sleep(3);
-      }
-    }
-    btrfs_scan();
-    if (mount_rw_loop_btrfs(datafile, "/mnt/rw", compress) != 0) {
-      printf("Failed to mount RW layer. Attempting repair.\n");
-      if (debug) sleep(3);
-      repair_btrfs_imagefile(datafile);
-      if (mount_rw_loop_btrfs(datafile, "/mnt/rw", compress) != 0) {
-        printf("No valid persistent RW layer. Using tmpfs.\n");
-        if (debug) sleep(3);
-      }
-    }
-  }
-
-  if (!is_mounted("/mnt/rw")) {
-    mount_or_die("tmpfs", "/mnt/rw", "tmpfs", MS_RELATIME, "");
-  }
-
-  mount_overlay_or_die("/mnt/system", "/mnt/rw/root", "/mnt/rw/work", "/newroot");
-  mount_or_die("tmpfs", "/newroot/run", "tmpfs", MS_NODEV|MS_NOSUID|MS_STRICTATIME, "mode=755");
-
-  move_mount_or_die("/mnt/boot", "/newroot/run/initramfs/boot");
-  move_mount_or_die("/mnt/system", "/newroot/run/initramfs/ro");
-  move_mount_or_die("/mnt/rw", "/newroot/run/initramfs/rw");
-
-  if (is_file("/newroot"FIRMWARE_ARCHIVE)) {
-    char hash[33];
-    const char* hash_file = "/newroot/lib/firmware/.hash";
-    printf("Firmware archive found. ");fflush(stdout);
-    if (md5("/newroot"FIRMWARE_ARCHIVE, hash, 1) == 0) {
-      char hash_prev[33] = "";
-      FILE *f;
-      printf("Extracting...");fflush(stdout);
-      f = fopen(hash_file, "r");
-      if (f) {
-        if (fread(hash_prev, 32, 1, f) == 1) {
-          hash_prev[32] = '\0';
-        } else {
-          hash_prev[0] = '\0';
-        }
-        fclose(f);
-      }
-      if (strcmp(hash, hash_prev) != 0) {
-        extract_archive("/newroot", FIRMWARE_ARCHIVE, "/lib/firmware", 1);
-        f = fopen(hash_file, "w");
-        if (f) {
-          fwrite(hash, 32, 1, f);
-          fclose(f);
-        }
-        printf("done.\n");
-      } else {
-        printf("Already done.\n");
-      }
-    } else {
-      printf("But it's broken.\n");
-      if (debug) sleep(3);
-    }
-  }
 
   if (hostname) {
     if (set_hostname("/newroot", hostname) == 0) {
@@ -116,23 +30,6 @@ void setup(inifile_t ini)
       printf("hostname: %s (generated)\n", default_hostname);
     } else {
       printf("Hostname setup failed.\n");
-      if (debug) sleep(3);
-    }
-  }
-
-  if (password) {
-    if (set_root_password("/newroot", password) == 0) {
-      printf("Root password configured.\n");
-    } else {
-      printf("Failed to set root password.\n");
-    }
-  }
-
-  if (!persistent && !is_file("/newroot/run/initramfs/rw/root/etc/shadow")) {
-    if (enable_autologin("/newroot") == 0) {
-      printf("Autologin enabled.\n");
-    } else {
-      printf("Autologin could not be enabled.\n");
       if (debug) sleep(3);
     }
   }
@@ -163,38 +60,12 @@ void setup(inifile_t ini)
       if (debug) sleep(3);
     }
   }
-
-  if (swap) {
-    const char* swapfile = "/newroot/run/initramfs/boot/efi/boot/bootx86.swp";
-    if (!exists(swapfile)) {
-      if (get_free_disk_space("/newroot/run/initramfs/boot") >= (uint64_t)1024*1024*1024*2 ) {
-        printf("Swapfile does not exist. Creating...");fflush(stdout);
-        if (create_swapfile(swapfile, 1024L*1024*1024) == 0) {
-          printf("done.\n");
-        } else {
-          printf("failed.\n");
-          if (debug) sleep(3);
-        }
-      } else {
-        printf("No sufficient disk space to create swapfile.");
-        if (debug) sleep(3);
-      }
-    }
-
-    if (is_file(swapfile)) {
-      printf("Activating swap...\n");
-      activate_swap(swapfile);
-    }
-  }
-
-  if (debug) {
-    puts("<<<<setup");
-    sleep(3);
-  }
 }
 
 void init()
 {
+  const char* datafile = "/mnt/boot/systsm.dat";
+  const char* swapfile = "/mnt/boot/system.swp";
   struct partition_struct partition;
   mount_procdevsys_or_die();
 
@@ -205,9 +76,52 @@ void init()
   printf("%s\n", partition.device);
 
   mount_or_die(partition.device, "/mnt/boot", "vfat", MS_RELATIME, "fmask=177,dmask=077");
-  mount_ro_loop_or_die("/mnt/boot/efi/boot/bootx86.efi", "/mnt/system", 1048576);
+  mount_ro_loop_or_die("/mnt/boot/system.img", "/mnt/system", 0);
 
-  process_inifile("/mnt/boot/efi/boot/bootx86.ini", setup);
+  if (!exists(datafile)) {
+    printf("RW layer does not exist. Creating...");fflush(stdout);
+    if (create_btrfs_imagefile(datafile, 109*1024*1024) == 0) {
+        printf("done.\n");
+    } else {
+        printf("failed.\n");
+        unlink(datafile);
+        sync();
+        halt();
+    }
+  }
+  btrfs_scan();
+  if (mount_rw_loop_btrfs(datafile, "/mnt/rw", 1/*compress*/) != 0) {
+    printf("Failed to mount RW layer. Attempting repair.\n");
+    repair_btrfs_imagefile(datafile);
+    if (mount_rw_loop_btrfs(datafile, "/mnt/rw", 1/*compress*/) != 0) {
+      printf("No valid persistent RW layer.\n");
+      halt();
+    }
+  }
+
+  if (!exists(swapfile)) {
+    printf("Swapfile does not exist. Creating...");fflush(stdout);
+    if (create_swapfile(swapfile, 1024L*1024*1024) == 0) {
+      printf("done.\n");
+    } else {
+      printf("failed.\n");
+      unlink(swapfile);
+    }
+  }
+
+  if (is_file(swapfile)) {
+    printf("Activating swap...\n");
+    activate_swap(swapfile);
+  }
+
+  mount_overlay_or_die("/mnt/system", "/mnt/rw/root", "/mnt/rw/work", "/newroot");
+  mount_or_die("tmpfs", "/newroot/run", "tmpfs", MS_NODEV|MS_NOSUID|MS_STRICTATIME, "mode=755");
+
+  move_mount_or_die("/mnt/boot", "/newroot/run/initramfs/boot");
+  move_mount_or_die("/mnt/system", "/newroot/run/initramfs/ro");
+  move_mount_or_die("/mnt/rw", "/newroot/run/initramfs/rw");
+
+  process_inifile("/newroot/run/initramfs/boot/system.ini", setup);
 
   setup_initramfs_shutdown("/newroot");
 
