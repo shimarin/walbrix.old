487f150fb76c537b285b1dea0fbd24399464b5ec
Tomoatsu Shimada <shimada@walbrix.com>
2020-12-15 20:17:12 +0900

    rename profile

--- profile/paravirt-gui/stage3-url.sh
+++
@@ -1,6 +0,0 @@
-BASEDIR=http://ftp.jaist.ac.jp/pub/Linux/Gentoo/releases/amd64/autobuilds/
-FILENAME=$(wget -q -O - ${BASEDIR}latest-stage3-amd64-systemd.txt | egrep -v '^#' | awk '{print $1}'|head -n 1) 
-if [ -z "$FILENAME" ]; then
-	exit 1
-fi
-echo ${BASEDIR}${FILENAME}
--- profile/paravirt/stage3-url.sh
+++ profile/paravirt/stage3-url.sh
@@ -1,5 +1,5 @@
 BASEDIR=http://ftp.iij.ad.jp/pub/linux/gentoo/releases/amd64/autobuilds/
-FILENAME=$(wget -q -O - ${BASEDIR}latest-stage3-amd64.txt | egrep -v '^#' | awk '{print $1}'|head -n 1) 
+FILENAME=$(wget -q -O - ${BASEDIR}latest-stage3-amd64-systemd.txt | egrep -v '^#' | awk '{print $1}'|head -n 1) 
 if [ -z "$FILENAME" ]; then
 	exit 1
 fi
